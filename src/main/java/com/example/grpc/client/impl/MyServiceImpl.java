203df6e311cbc3c3dc023a3bab4ddd8b35051741
齐炳旭 <2395461672@qq.com>
2022-11-15 10:44:02 +0800

    first commot

---
+++ src/main/java/com/example/grpc/client/impl/MyServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.grpc.client.impl;
+
+import com.example.grpc.client.HelloReply;
+import com.example.grpc.client.HelloRequest;
+import com.example.grpc.client.SimpleGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class MyServiceImpl extends SimpleGrpc.SimpleImplBase {
+
+    @Override
+    public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+        HelloReply reply = HelloReply.newBuilder()
+                .setMessage("Hello ==> " + request.getName())
+                .build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+    }
+
+}
